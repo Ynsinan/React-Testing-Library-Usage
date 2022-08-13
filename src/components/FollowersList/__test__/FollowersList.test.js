744b45491905cd9ff23dc2b528b35fefbefe26bc
Yunusinan <yunus.inan@arutesolutions.com>
2022-08-13 16:21:30 +0300

    FollowerList Test Added

---
+++ src/components/FollowersList/__test__/FollowersList.test.js
@@ -0,0 +1,28 @@
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import FollowersList from "../FollowersList";
+
+const MockFollowersList = () => {
+  return (
+    <BrowserRouter>
+      <FollowersList />
+    </BrowserRouter>
+  );
+};
+describe("FollowersList", () => {
+  it("should renders follower items", async () => {
+    render(<MockFollowersList />);
+    //await getByTestId ile çalışmaz
+    // const followerDivElement = await screen.getByTestId("follower-item-0");
+    const followerDivElement = await screen.findByTestId("follower-item-0");
+    expect(followerDivElement).toBeInTheDocument();
+  });
+
+  it("should render multiple follower items", async () => {
+    render(<MockFollowersList />);
+    //await getByTestId ile çalışmaz
+    // const followerDivElement = await screen.getByTestId("follower-item-0");
+    const followerDivElement = await screen.findAllByTestId(/follower-item/i);
+    expect(followerDivElement.length).toBe(5);
+  });
+});
